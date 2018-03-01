ef16eca98d85ef25ebf0fbdc23f88273cc6a42a6
adddward <adddward@gmail.com>
2018-03-01 13:17:55 +0300

    Function to compare two arrays
    *Function to compare two arrays was added
    *Some shit was deleted

---
+++ src/Queen.java
@@ -0,0 +1,14 @@
+/**
+ * Created by adddw on 27.02.2018.
+ */
+public class Queen extends Checker{
+
+    public Queen(byte position[], int color){
+        super(position, color);
+    }
+
+    @Override
+    public int[][] chop(int[][] field ,byte[] place_of_chop) {
+    return field;
+    }
+}
--- src/Test_Checker.java
+++ src/Test_Checker.java
@@ -119,8 +119,20 @@ public class Test_Checker {
                     {0, 0, 0, 0, 0, 0, 0, 0}
             }
     };
+
+    static boolean compareFields(int[][] field1, int[][] field2){
+        if (field1.length != field2.length || field1[0].length != field2[0].length) return false;
+
+        for (int i = 0; i < field1.length; i++){
+            for (int k = 0; k < field1[0].length; k++){
+                if (field1[i][k] != field2[i][k]) return false;
+            }
+        }
+        return true;
+    }
+
     static public boolean Test (Checker checker, int[][] field, byte[] chop,byte[] place_of_checker,int[][] result) {
-        return checker.chop(field, chop) == result;}
+        return compareFields(checker.chop(field, chop), result);}
 
     public static void main(String args[]){
         for(int i =0;i<5;i++){
