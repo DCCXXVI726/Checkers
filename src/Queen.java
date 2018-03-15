500905f11bdf2e2eaea8cc9192ca0f8d4329a939
adddward <adddward@gmail.com>
2018-03-15 20:28:52 +0300

    Move method for queen class

--- src/Queen.java
+++ src/Queen.java
@@ -1,32 +1,62 @@
-public class Queen {
-    public byte color;
-    public byte[] position;
+public class Queen extends Checker {
 
-    Queen(byte[] position, byte color) {
-        this.position = position;
-        this.color = color;
+    Queen(byte[] position1, byte color1) {
+        super(position1, color1);
     }
 
-    public void chop(byte[][] field, byte[] placeOfChop, byte[] finalPosition) {
+    public byte[][] chop(byte[][] field, byte[] place_of_chop) {
+        if (Math.abs(place_of_chop[0] - position[0]) != Math.abs(place_of_chop[1] - position[1])) return field;
 
-        int xStep = (finalPosition[0] - position[0]);
+        byte[] enemies = countEnemies(field, place_of_chop);
+
+        if (enemies[0] > 1 || enemies[1] != 1) return field;
+        else if (field[place_of_chop[0]][place_of_chop[1]] == 0) {
+            field[enemies[2]][enemies[3]] = 0;
+            field[place_of_chop[0]][place_of_chop[1]] = color;
+            field[position[0]][position[1]] = 0;
+            position[0] = place_of_chop[0];
+            position[1] = place_of_chop[1];
+        }
+        return field;
+    }
+
+    public byte[][] move(byte[][] field, byte[] place_of_move) {
+        if (Math.abs(place_of_move[0] - position[0]) != Math.abs(place_of_move[1] - position[1])) return field;
+
+        byte[] enemies = countEnemies(field, place_of_move);
+
+        if (enemies[0] > 0) return field;
+        else if (field[place_of_move[0]][place_of_move[1]] == 0) {
+            field[place_of_move[0]][place_of_move[1]] = color;
+            field[position[0]][position[1]] = 0;
+            position[0] = place_of_move[0];
+            position[1] = place_of_move[1];
+        }
+        return field;
+    }
+
+    private byte[] countEnemies(byte[][] field, byte[] finalPosition) {
+        int xStep = finalPosition[0] - position[0];
         xStep = xStep / Math.abs(xStep);
-        int yStep = (finalPosition[1] - position[1]);
+        int yStep = finalPosition[1] - position[1];
         yStep = yStep / Math.abs(yStep);
+        byte xVictimPos = 0, yVictimPos = 0;
+        byte numOfVictims = 0, numOfCheckers = 0;
 
-        int numOfVictims = 0;
-
-        for (int i = 0; i < finalPosition[0]; i += xStep) {
-            for (int k = 0; k < finalPosition[1]; k += yStep) {
-                if (field[i][k] == color * (-1)) numOfVictims++;
+        for (int b = 1; finalPosition[0] - b * xStep - position[0] != 0; b++) {
+            int x = position[0] + b * xStep;
+            int y = position[1] + b * yStep;
+            if (field[x][y] != 0) {
+                numOfCheckers += 1;
+                if (field[x][y] == color * (-1)) {
+                    numOfVictims++;
+                    xVictimPos = (byte) x;
+                    yVictimPos = (byte) y;
+                }
             }
         }
 
-        if (numOfVictims > 1) return;
-        else if (numOfVictims == 1 && field[finalPosition[0]][finalPosition[1]] == 0 && field[placeOfChop[0]][placeOfChop[1]] == color * (-1)){
-            field[placeOfChop[0]][placeOfChop[1]] = 0;
-            field[finalPosition[0]][finalPosition[1]] = color;
-            field[position[0]][position[1]] = 0;
-        }
+        byte[] result = {numOfCheckers, numOfVictims, xVictimPos, yVictimPos};
+        return result;
     }
 }
--- src/Test_Queen.java
+++ src/Test_Queen.java
@@ -32,12 +32,6 @@ public class Test_Queen {
             }
     };
 
-    static byte[][] place_of_chop = {
-            {2, 2},
-            {2, 2},
-            {4, 3},
-    };
-
     static byte[][] place_of_checker = {
             {1, 1},
             {1, 1},
@@ -83,15 +77,101 @@ public class Test_Queen {
             }
     };
 
-    public static boolean Test(Queen queen, byte number_of_case) {
-        queen.chop(test_field[number_of_case], place_of_chop[number_of_case], place_of_final_position[number_of_case]);
+    static byte[][][] test_field_move = {
+            {
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, -1, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 1, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0}
+            },
+            {
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 1, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 1, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0}
+            },
+            {
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 1, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0}
+            },
+    };
+
+    static byte[][] place_of_checker_move = {
+            {3, 1},
+            {5, 1},
+            {5, 1},
+    };
+
+    static byte[][] place_of_final_position_move = {
+            {0, 4},
+            {2, 4},
+            {0, 6},
+    };
+
+    static byte[][][] result_move = {
+            {
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, -1, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 1, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0}
+            },
+            {
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 1, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 1, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0}
+            },
+            {
+                    {0, 0, 0, 0, 0, 0, 1, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0}
+            },
+    };
+
+    public static boolean TestChop(Queen queen, byte number_of_case) {
+        queen.chop(test_field[number_of_case], place_of_final_position[number_of_case]);
         return Test_Checker.compareFields(test_field[number_of_case], result[number_of_case]);
     }
 
-    public static void main(String[] args){
-        for (byte k = 0; k < 3; k ++) {
+    public static boolean TestMove(Queen queen, byte number_of_case) {
+        queen.move(test_field_move[number_of_case], place_of_final_position_move[number_of_case]);
+        return Test_Checker.compareFields(test_field_move[number_of_case], result_move[number_of_case]);
+    }
+
+    public static void main(String[] args) {
+        for (byte k = 0; k < 3; k++) {
             Queen queen = new Queen(place_of_checker[k], (byte) 1);
-            System.out.println(Test(queen, k));
+            Queen queenTestMove = new Queen(place_of_checker_move[k], (byte) 1);
+
+            System.out.println("Chop:" + TestChop(queen, k));
+            System.out.println("Move:" + TestMove(queenTestMove, k));
         }
     }
 }
