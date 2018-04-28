b5034ceec8e0f0851f4ae75fbdb8344ff77c51d2
DCCXXVI726 <Dccxxvi726726@gmail.com>
2018-04-28 19:38:34 +0300

    Crutch

--- src/Checker.java
+++ src/Checker.java
@@ -1,10 +1,7 @@
-public class Checker {
-    public byte color;
-    public byte[] position= new byte[2];
-    Checker(byte[] position1,byte color1){
-        color = color1;
-        position[0] = position1[0];
-        position[1] = position1[1];
+public class Checker extends Queen {
+
+    Checker(byte[] position1, byte color1) {
+        super(position1, color1);
     }
     public boolean chop(byte[][] field ,byte[] place_of_chop){
         if (Math.abs(place_of_chop[0]-position[0])==2
@@ -13,6 +10,8 @@ public class Checker {
                 &&field[place_of_chop[0]][place_of_chop[1]]==0){
             field[place_of_chop[0]][place_of_chop[1]]=color;
             field[(place_of_chop[0]-position[0])/2+position[0]][(place_of_chop[1]-position[1])/2+position[1]]=0;
+            position_of_chop[0] = (byte)((place_of_chop[0]-position[0])/2+position[0]);
+            position_of_chop[1] = (byte)((place_of_chop[1]-position[1])/2+position[1]);
             field[position[0]][position[1]]=0;
             position[0]=place_of_chop[0];
             position[1]=place_of_chop[1];
