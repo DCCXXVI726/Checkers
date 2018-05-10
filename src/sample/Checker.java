4cc434563093195aaaeaed1a4d02227ca47caebb
DCCXXVI726 <Dccxxvi726726@gmail.com>
2018-05-10 18:54:25 +0300

    Fix bug with chop

--- src/sample/Checker.java
+++ src/sample/Checker.java
@@ -19,10 +19,10 @@ public class Checker {
             field[place_of_chop[0]][place_of_chop[1]] = color;
             field[(place_of_chop[0] - position[0]) / 2 + position[0]][(place_of_chop[1] - position[1]) / 2 + position[1]] = 0;
             field[position[0]][position[1]] = 0;
-            position[0] = place_of_chop[0];
-            position[1] = place_of_chop[1];
             chopped[0] = (byte)((place_of_chop[0] - position[0]) / 2 + position[0]);
             chopped[1] = (byte)((place_of_chop[1] - position[1]) / 2 + position[1]);
+            position[0] = place_of_chop[0];
+            position[1] = place_of_chop[1];
             return true;
         } else {
             return false;
