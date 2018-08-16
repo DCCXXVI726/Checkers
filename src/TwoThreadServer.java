e51beb83dc9cc403850895d4efa3d0f9a730a7b9
DCCXXVI726 <Dccxxvi726726@gmail.com>
2018-08-16 17:49:55 +0300

    add "interface" MonoTheardClient

---
+++ src/TwoThreadServer.java
@@ -0,0 +1,31 @@
+public class MonoThreadClientServer {
+    /**
+     * Start server
+     */
+    public void start(){
+
+        };
+
+    /**
+     * Stop server
+     */
+    public void stop(){
+
+    };
+
+    /**
+     * receive positions of moving checker;
+     * @return changing position;
+     */
+    public int[][] receive (String positions){
+        return;
+    }
+    /**
+     *
+     * @param field - Changed field
+     * @return field to String
+     */
+    public String send (int[][] field){
+        return;
+    }
+}
